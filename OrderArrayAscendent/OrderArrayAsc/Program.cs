a1a28cedf3c653833d720410d3b89fbb6befae93
Emilia Carvajal <emilia.carvajalm@gmail.com>
2022-12-24 20:22:50 -0400

    Adding code for ordering array ascendent on dev

---
+++ OrderArrayAscendent/OrderArrayAsc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program {
+static void Main(string[] args)
+{
+  int[] numeros = { 15, 1, 34, 5, 90, 10, 6 };
+  Console.WriteLine("Antes de ordenar");
+  foreach (int numero in numeros)
+  {
+    Console.Write(numero + ",");
+  }
+  Console.WriteLine("");
+  ordenarBurbujaAscendente(numeros);
+  Console.WriteLine("Después de ordenar ascendente");
+  foreach (int numero in numeros)
+  {    
+    Console.Write(numero + ",");
+  }
+  Console.WriteLine();
+}
+static void ordenarBurbujaAscendente(int[] array)
+{
+     for (int x = 0; x < array.Length; x++)
+     {
+        for (int indiceActual = 0; indiceActual < array.Length - x -1;indiceActual++)
+        {
+          int indiceSiguienteElemento = indiceActual + 1;
+          // Si el actual es mayor que el que le sigue a la derecha...
+          if (array[indiceActual] > array[indiceSiguienteElemento])
+           {
+              int temporal = array[indiceActual];
+              array[indiceActual] = array[indiceSiguienteElemento];
+               array[indiceSiguienteElemento] = temporal;
+            }
+         }
+      }   
+}
+}
